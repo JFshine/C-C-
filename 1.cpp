97c3202c41a29927192b4df9cf6d91e3ca5df342
CodeJ <zhuyutiandi@163.com>
2016-10-13 17:47:32 +0800

    add file 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: wsf
+	> Mail: wusf@farsight.cn 
+	> Created Time: 2016年10月12日 星期三 16时23分42秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+	cout <<"Hello world!"<< endl;
+
+	return 0;
+}
+
